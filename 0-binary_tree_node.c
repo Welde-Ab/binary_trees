82dcdf86b0b50baae453ffb885320911b4425614
Welde-Ab <welde@DESKTOP-G35A4D3.localdomain>
2022-08-26 21:55:46 +0900

    another big commit

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,28 +1,25 @@
-#include <stdlib.h>
 #include "binary_trees.h"
 
 /**
- * binary_tree_node - Stores recursively each level in an array of strings
+ * binary_tree_node - Creates a binary tree node.
+ * @parent: A pointer to the parent of the node to create.
+ * @value: The value to put in the new node.
  *
- * @parent: Pointer to the node to parent node
- * @value: Value to be assigned
- *
- * Return: The the create node
+ * Return: If an error occurs - NULL.
+ *         Otherwise - a pointer to the new node.
  */
-
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-binary_tree_t *node = malloc(sizeof(binary_tree_t));
-if (node != NULL)
-{
-node->n = value;
-node->parent = parent;
-node->left = node->right = NULL;
-return (node);
-}
+	binary_tree_t *new_node;
 
-else
-{
-return (NULL);
-}
+	new_node = malloc(sizeof(binary_tree_t));
+	if (!new_node)
+		return (NULL);
+
+	new_node->parent = parent;
+	new_node->n = value;
+	new_node->left = NULL;
+	new_node->right = NULL;
+
+	return (new_node);
 }
